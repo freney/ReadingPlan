7c9431a9ef2cedd8e359e0dfea264925f57dfe60
Samuel Freney <samf@home.freney.org>
2011-05-10 16:10:01 +1000

    Initial Commit

---
+++ Reading Plan/Reading_PlanAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  Reading_PlanAppDelegate.h
+//  Reading Plan
+//
+//  Created by Samuel Freney on 10/05/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Reading_PlanAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+
+@end
---
+++ Reading Plan/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  Reading Plan
+//
+//  Created by Samuel Freney on 10/05/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UITableViewController {
+
+}
+
+
+@end
